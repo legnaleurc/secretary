224073242a80c467cd35b47f08a6258cbb062dd1
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-10-02 00:19:56 +0900

    fix

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,11 @@
+if __name__ == '__main__' and __package__ == '':
+    import os, sys, importlib
+    parent_dir = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(os.path.dirname(parent_dir))
+    __package__ = os.path.basename(parent_dir)
+    importlib.import_module(__package__)
+
+
 import sys
 import functools
 import re
@@ -202,7 +210,7 @@ class FallbackHandler(object):
         self._chat_buffer = collections.deque(maxlen=16)
 
     @gen.coroutine
-    @command_filter(r'^s([^\r\n\\]{4,})$')
+    @command_filter(r'^s([^\r\n\\].{3,})$')
     def sed(self, message, *args, **kwargs):
         tmp = args[0]
         delimiter, tmp = tmp[0], tmp[1:]
