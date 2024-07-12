ece67ae5792ae44af3796f13824cdfbeb7e0556f
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-07-13 02:03:53 +0900

    feat: add dvd list

--- bot/context.py
+++ bot/context.py
@@ -2,13 +2,26 @@ import os
 from dataclasses import dataclass
 
 
+type DvdList = list[tuple[str, str]]
+
+
 @dataclass(frozen=True, kw_only=True)
 class Context:
     api_token: str
+    dvd_list: DvdList
 
 
 def get_context():
     api_token = os.environ.get("API_TOKEN", "")
+    dvd_list = os.environ.get("DVD_LIST", "")
     if not api_token:
         raise RuntimeError("`API_TOKEN` environment variable missing")
-    return Context(api_token=api_token)
+    return Context(api_token=api_token, dvd_list=parse_dvd_list(dvd_list))
+
+
+def parse_dvd_list(dvd_list: str) -> DvdList:
+    if not dvd_list:
+        return []
+    pair_list = dvd_list.split(" ")
+    raw_list = (_.split("|") for _ in pair_list)
+    return [(_[0], _[1]) for _ in raw_list]
