d60547bfc471d3d6b6b7dc5447236850607b9702
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-08-04 21:23:36 +0900

    feat: select original

--- bot/avid/dmm.py
+++ bot/avid/dmm.py
@@ -1,7 +1,9 @@
 import re
 from logging import getLogger
+from pathlib import PurePath
 from urllib.parse import urlsplit, urlunsplit
 
+from bs4 import Tag
 from telegram import LinkPreviewOptions
 
 from bot.lib import get_html, make_av_keyboard
@@ -40,22 +42,31 @@ def parse_avid(unknown_text: str) -> str:
 async def get_url(avid: str) -> str:
     soup = await get_html(f"https://www.dmm.co.jp/search/=/searchstr={avid}/")
 
-    anchor = soup.select_one(".txt > a:nth-child(1)")
-    if not anchor:
+    anchor_list = soup.select(".txt > a")
+    if not anchor_list:
         return ""
 
+    flat_id = avid.replace("-", "").lower()
+
+    pair_list = sorted(filter(None, (get_cid_and_url(_, flat_id) for _ in anchor_list)))
+    original = pair_list[0][-1]
+    return original
+
+
+def get_cid_and_url(anchor: Tag, flat_id: str) -> tuple[str, str] | None:
     raw_url = anchor.get("href")
     if not raw_url:
-        return ""
+        return None
     if not isinstance(raw_url, str):
-        return ""
+        return None
 
     try:
         parsed_url = urlsplit(raw_url)
     except Exception:
         _L.exception(f"invalid url {raw_url}")
-        return ""
+        return None
 
+    cid = get_cid(parsed_url.path, flat_id)
     url = urlunsplit(
         (
             parsed_url.scheme,
@@ -65,4 +76,11 @@ async def get_url(avid: str) -> str:
             "",
         )
     )
-    return url
+    return cid, url
+
+
+# The original should have no prefix, should be the first after sorting.
+def get_cid(url_path: str, flat_id: str) -> str:
+    path = PurePath(url_path)
+    last = path.parts[-1]
+    return last.replace("cid=", "").replace(flat_id, "")
