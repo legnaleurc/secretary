cd3ffe510563e7068bde438bc7f10c101d55018b
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-07-18 22:59:46 +0900

    _ api

--- bot/daemon/api.py
+++ bot/daemon/api.py
@@ -1,7 +1,7 @@
 from collections.abc import Awaitable, Callable
 from contextlib import asynccontextmanager
-from functools import wraps
-from typing import TypedDict
+from functools import partial, wraps
+from typing import Concatenate, TypedDict
 
 from aiohttp.web import (
     AppKey,
@@ -28,8 +28,6 @@ type WebhookCallback = Callable[[dict[str, object]], Awaitable[None]]
 TOKEN_HEADER = "X-Telegram-Bot-Api-Secret-Token"
 
 KEY_TOKEN = AppKey[str]("KEY_TOKEN")
-KEY_ENQUEUE = AppKey[EnqueueCallback]("KEY_ENQUEUE")
-KEY_WEBHOOK = AppKey[WebhookCallback]("KEY_WEBHOOK")
 
 
 @asynccontextmanager
@@ -39,13 +37,13 @@ async def api_daemon(
     app = Application()
 
     app[KEY_TOKEN] = context.client_token
-    app[KEY_ENQUEUE] = enqueue
 
-    app.router.add_route("POST", "/api/v1/text", _handle_text)
+    app.router.add_route("POST", "/api/v1/text", partial(_handle_text, enqueue=enqueue))
 
     if webhook and context.webhook_path:
-        app[KEY_WEBHOOK] = webhook
-        app.router.add_route("POST", context.webhook_path, _handle_webhook)
+        app.router.add_route(
+            "POST", context.webhook_path, partial(_handle_webhook, webhook=webhook)
+        )
 
     runner = AppRunner(app)
     await runner.setup()
@@ -58,14 +56,16 @@ async def api_daemon(
         await runner.cleanup()
 
 
-def _token_required(
-    fn: Callable[[Request], Awaitable[Response]],
-) -> Callable[[Request], Awaitable[Response]]:
+def _token_required[**P](
+    fn: Callable[Concatenate[Request, P], Awaitable[Response]],
+) -> Callable[Concatenate[Request, P], Awaitable[Response]]:
     @wraps(fn)
-    async def _assert_token(request: Request) -> Response:
+    async def _assert_token(
+        request: Request, *args: P.args, **kwargs: P.kwargs
+    ) -> Response:
         if not _is_valid(request):
             raise HTTPUnauthorized()
-        return await fn(request)
+        return await fn(request, *args, **kwargs)
 
     return _assert_token
 
@@ -79,9 +79,7 @@ def _is_valid(request: Request) -> bool:
 
 
 @_token_required
-async def _handle_text(request: Request) -> Response:
-    enqueue = request.app[KEY_ENQUEUE]
-
+async def _handle_text(request: Request, *, enqueue: EnqueueCallback) -> Response:
     data: TextData = await request.json()
     chat_id = data["chat_id"]
     text = data["text"]
@@ -92,9 +90,7 @@ async def _handle_text(request: Request) -> Response:
 
 
 @_token_required
-async def _handle_webhook(request: Request) -> Response:
-    webhook = request.app[KEY_WEBHOOK]
-
+async def _handle_webhook(request: Request, *, webhook: WebhookCallback) -> Response:
     data: dict[str, object] = await request.json()
 
     await webhook(data)
